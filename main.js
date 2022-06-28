3dcb004beaa74ed40651430cd8c06ed0acda3663
Sardorxoja21 <sardorrixsixojayev315@gmail.com>
2022-06-28 20:17:58 +0500

    online

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ $.ajax({
 })
 
 function Element1(asus) {
+    $(".cardlar1").html("")
 asus.map((v,i)=>{
     let card = `
     <div class="col-4">
     <div class="card mt-5">
+       <p class="ps-3 pt-3 fs-4" id="like" onclick="like()"><i class="bi bi-heart"></i></p>
         <img class"cardimg shadow-lg ms-3" src="${v.img}" alt="">
-        <p></p>
         <a href="" class="link text-center mt-3">${v.name}</a>
         <div class="btn1 mt-3">
-        <button class="korzinkastyle btn btn-primary">Korzina</button>
+        <button class="korzinkastyle btn btn-primary" onclick="addTocard()" id="add">Korzina</button>
         </div>
     </div>
  </div>
@@ -32,6 +33,13 @@ asus.map((v,i)=>{
 })
 }
 
+let card =[];
+const count=0
+$("#add").on("click",()=>{
+    ++count==1
+    console.log(salom);
+})
+
 $("#searc").on("input",()=>{
     let yangimassiv = boshmassiv.filter(soz=>{
         return soz.name.toLowerCase().includes($("#searc").val().toLowerCase())
@@ -39,3 +47,8 @@ $("#searc").on("input",()=>{
     Element1(yangimassiv)
     console.log($("#searc").val());
 })
+
+function like() {
+    $("#like").toogleClass("btn-light")
+    $("#like").toogleClass("btn-danger")
+}
